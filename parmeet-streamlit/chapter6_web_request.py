a74d953ddf518f9d4a93dd8b78589c9a7e730794
granthiparmeet-gif <granthi.parmeet@gmail.com>
2025-09-27 22:00:55 +0530

    New changes

--- parmeet-streamlit/chapter6_web_request.py
+++ parmeet-streamlit/chapter6_web_request.py
@@ -7,7 +7,7 @@ amount = st.number_input("Enter the amount in INR")
 
 target_cuurency = st.selectbox("Convert to:", ["USD","EUR", "JPY", "GBP" ])
 
-if st.button("Convert"):
+if st.button("Convert Now"):
     url = "https://v6.exchangerate-api.com/v6/f7115bbc2c6f0db9e27806cf/latest/INR"
     response = requests.get(url)
 
